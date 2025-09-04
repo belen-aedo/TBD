78feadbbe59474442c81ce31d4279c55628fea33
Javiera <javiera.guajardo.g@usach.cl>
2025-09-04 09:01:30 -0400

    Se agregaron consultas 9 y 10

--- runStatemes.sql
+++ runStatemes.sql
@@ -12,6 +12,8 @@ WHERE c.nacionalidad = 'Chilena'
 GROUP BY anio, v.destino
 ORDER BY anio, total_viajes DESC;
 
+
+
 -- 2) Lista con las secciones de vuelos más compradas por argentinos.
 
 SELECT
@@ -23,3 +25,72 @@ JOIN SECCION s ON s.id_seccion   = p.id_seccion
 WHERE c.nacionalidad = 'Argentina'
 GROUP BY s.tipo_seccion
 ORDER BY num_compras DESC;
+
+
+
+-- 9) Lista anual de compañias que en promedio han pagado más a sus empleados (últimos 10 años)
+
+-- Primero se calcula el promedio salarial por compañia y año
+WITH promedio_anual_por_compania AS (
+    SELECT 
+        c.nombre AS compania,
+        EXTRACT(YEAR FROM s.fecha_pago) AS anio,
+        AVG(s.monto_pago) AS promedio_sueldo_anual
+    FROM SUELDO s
+    JOIN EMPLEADO e ON s.rut_e = e.rut_e
+    JOIN COMPANIA c ON e.compania_id = c.compania_id
+    WHERE s.fecha_pago >= CURRENT_DATE - INTERVAL '10 years'
+    GROUP BY c.nombre, EXTRACT(YEAR FROM s.fecha_pago)
+),
+-- Aqui se asigna un ranking a cada compañia por año *y se crea también la columna ranking
+ranking_anual AS (
+    SELECT 
+        anio,
+        compania,
+        promedio_sueldo_anual,
+        -- ROW_NUMBER() genera números secuenciales (1,2,3...) para cada año
+        -- PARTITION BY anio: reinicia la numeración para cada año
+        -- ORDER BY promedio_sueldo_anual DESC: ordena de mayor a menor promedio
+        ROW_NUMBER() OVER (PARTITION BY anio ORDER BY promedio_sueldo_anual DESC) AS ranking
+    FROM promedio_anual_por_compania
+)
+-- Se muestran solo las compañias con ranking = 1 que es la que mas paga en el año
+SELECT 
+    anio,
+    compania,
+    promedio_sueldo_anual,
+    ranking  -- Esta columna YA EXISTE porque se creó en el CTE anterior
+FROM ranking_anual
+WHERE ranking = 1
+ORDER BY anio DESC;
+
+
+-- 10) Modelo de avión más usado por compañía durante el 2021
+
+WITH vuelos_2021_por_modelo AS (
+    SELECT 
+        c.nombre AS compania,
+        m.nombre_modelo,
+        COUNT(v.id_vuelo) AS cantidad_vuelos
+    FROM VUELO v
+    JOIN AVION a ON v.id_avion = a.id_avion
+    JOIN MODELO m ON a.id_modelo = m.id_modelo
+    JOIN COMPANIA c ON v.compania_id = c.compania_id
+    WHERE EXTRACT(YEAR FROM v.fecha_vuelo) = 2021
+    GROUP BY c.nombre, m.nombre_modelo
+),
+ranking_modelos_por_compania AS (
+    SELECT 
+        compania,
+        nombre_modelo,
+        cantidad_vuelos,
+        ROW_NUMBER() OVER (PARTITION BY compania ORDER BY cantidad_vuelos DESC) AS ranking
+    FROM vuelos_2021_por_modelo
+)
+SELECT 
+    compania,
+    nombre_modelo AS modelo_mas_usado,
+    cantidad_vuelos
+FROM ranking_modelos_por_compania
+WHERE ranking = 1
+ORDER BY compania;
